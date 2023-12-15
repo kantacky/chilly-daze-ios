b9061e55a049bbb418bf2d92ccc1d96d5ee6cba4
Kanta Oikawa <k.oikawa@kantacky.com>
2023-12-15 23:07:12 +0900

    Update ci_scripts

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
 defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
-echo $(ls /Volumes/workspace/repository)
-echo $GOOGLE_SERVICE_INFO > /Volumes/workspace/repository/ChillyDaze/Sources/ChillyDaze/Resources/GoogleService-Info.plist
